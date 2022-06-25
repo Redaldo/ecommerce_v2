4cb3e9d49b460c08f1948fd4c93199ce4a9e7c7d
Ridha Herbache <redaldo2004@yahoo.co.uk>
2022-06-25 23:36:21 +0100

    seeders and factories ok

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Category;
 use Illuminate\Http\Request;
+use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
@@ -14,7 +15,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        
+        return CategoryResource::collection(Category::all());
     }
 
     /**
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'slug' => $faker->name,
+        'description' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+        'price' => $faker->numberBetween($min = 100, $max = 9000),
+        'image' => '5dfh4f455sdggdsfgsdfdfgfgdfg.jpg',
+        'category_id' => $faker->numberBetween($min = 1, $max = 6),
+        'brand_id' => $faker->numberBetween($min = 1, $max = 6)
     ];
 });
